43b9a79cb32d39d701c6ffcde47bdcc39e1f1044
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-21 21:46:37 +0200

    Create Program.cs

---
+++ Activities/API/Program.cs
@@ -0,0 +1,35 @@
+using API.Controllers;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
