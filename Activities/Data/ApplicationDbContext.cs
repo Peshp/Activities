b0577cdd71e7d5e9189fc7e6f0529c2e3f7f11be
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-21 21:46:24 +0200

    Create ApplicationDbContext.cs

---
+++ Activities/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Configuration;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ActivityEntityConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
