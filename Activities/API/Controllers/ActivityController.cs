cdea19d5e16835bd283120e006c4ff3c4e056ab9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-22 14:44:24 +0200

    Create ActivityController.cs

---
+++ Activities/API/Controllers/ActivityController.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivityController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Activity>>> All()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Activity>> GetById(Guid Id)
+        {
+            return await _context.Activities.FindAsync(Id);
+        }
+    }
+}
