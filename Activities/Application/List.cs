5d922c5ef4b9f8a52be608791bb69794f7b72432
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-23 19:08:34 +0200

    Create List.cs

---
+++ Activities/Application/List.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using Data.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
