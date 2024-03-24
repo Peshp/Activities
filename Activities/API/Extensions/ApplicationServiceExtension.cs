deb492e10a211e0637b444cf57df07d875eab384
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-24 05:01:30 +0200

    Create ApplicationServiceExtension.cs

---
+++ Activities/API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,31 @@
+﻿using Application;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173");
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+
+            return services;
+        }
+    }
+}
