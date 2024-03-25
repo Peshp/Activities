41c2da11197c3c8b106b875dd76145513b343fc8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-25 19:40:28 +0200

    Create Edit.cs

---
+++ Activities/Application/Edit.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using Data.Entities;
+using MediatR;
+
+namespace Application
+{
+    public class Edit
+    {
+        public class Query : IRequest
+        {
+            public Query(Activity activity)
+            {
+                Activity = activity;
+            }
+
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query>
+        {
+            private ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Query request, CancellationToken cancellationToken)
+            {
+                Activity change = await _context.Activities.FindAsync(request.Activity.Id);
+
+                await Mapper(request, change);
+
+                await _context.SaveChangesAsync();
+            }
+
+            private async Task Mapper(Query request, Activity change)
+            {
+                change.Name = request.Activity.Name;
+                change.City = request.Activity.City;
+                change.Description = request.Activity.Description;
+                change.Category = request.Activity.Category;
+                change.Date = request.Activity.Date;
+                change.Venue = request.Activity.Venue;
+            }
+        }
+    }
+}
