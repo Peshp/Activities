496f5136b420fafb49592de0b33c3c6b7f1d2ec2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-25 19:40:25 +0200

    Create Details.cs

---
+++ Activities/Application/Details.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+using Data.Entities;
+using MediatR;
+
+namespace Application
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Query(Guid id)
+            {
+                Id = id;
+            }
+
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.FindAsync(request.Id);
+            }
+        }
+    }
+}
