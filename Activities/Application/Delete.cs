e3e4bf3c67a14fcef2c11221b2a9be9a6caca162
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-25 19:40:22 +0200

    Create Delete.cs

---
+++ Activities/Application/Delete.cs
@@ -0,0 +1,38 @@
+﻿using Data;
+using Data.Entities;
+using MediatR;
+
+namespace Application
+{
+    public class Delete
+    {
+        public class Query : IRequest
+        {
+            public Query(Guid id)
+            {
+                Id = id;
+            }
+
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query>
+        {
+            private ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Query request, CancellationToken cancellationToken)
+            {
+                Activity remove = await _context.Activities.FindAsync(request.Id);
+
+                _context.Activities.Remove(remove);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
