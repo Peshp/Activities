ce648be95df35a01a4a906e6055d9ce3c95130da
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-26 23:05:33 +0200

    Create Activities.tsx

---
+++ client/src/app/layoult/Activities.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import MenuList  from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import { Activity } from "../models/Activity";
+import NavBar from "../Navbar/NavBar";
+
+function Activities(){
+    const [activities, setActivities] = useState<Activity[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5004/api/Activity')
+            .then(response => {
+                setActivities(response.data);
+            })
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            <h1 >Activities</h1>
+            <MenuList>
+                {activities.map((activity: any) => {
+                    return (
+                        <MenuItem key={activity.id}>
+                            {activity.name}
+                        </MenuItem>
+                    );
+                })}
+            </MenuList>
+        </div>
+    );
+}
+
+export default Activities;
