9448db9b1b08bfe9d0bf2893b87bcadd08b38779
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-30 00:47:00 +0200

    Update ActivityDetails.tsx

--- client/src/features/features/activities/details/ActivityDetails.tsx
+++ client/src/features/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,28 @@
-import { Card } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
 
-function ActivityDetails(){
+interface Props {
+    activity: Activity;
+}
+
+function ActivityDetails({activity}: Props){
     return (
-        <Card>
-            
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+            <Card.Content>
+                <Card.Header>{activity.name}</Card.Header>
+                <Card.Meta>
+                    <span>{activity.date}</span>
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+                </Card.Description>
+                <Card.Content extra>
+                    <Button.Group widths='2'>
+                        <Button basic color='blue' content='Edit' />
+                        <Button basic color='grey' content='Cancel' />
+                    </Button.Group>
+                </Card.Content>
+            </Card.Content>
         </Card>
     )
 }
