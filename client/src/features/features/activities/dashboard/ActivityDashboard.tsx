1fc52d216b323ba5f7a73f5d57cd8348ca0367aa
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-31 00:59:04 +0200

    Update ActivityDashboard.tsx

--- client/src/features/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/features/activities/dashboard/ActivityDashboard.tsx
@@ -5,10 +5,12 @@ import ActivityList from "./ActivityList";
 import { Activity } from "../../../../app/models/Activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
+import NavBar from "../../../../app/Navbar/NavBar";
 
 function Activities() {
     const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+    const [editMode, setEditMode] = useState(false);
 
     useEffect(() => {
         axios.get<Activity[]>('http://localhost:5004/api/Activity')
@@ -26,16 +28,32 @@ function Activities() {
         setSelectedActivity(undefined);
     }
 
+    function handleFormOpen(id?: string) {
+        id ? handleSelectActivity(id) : handleCancelSelectActivity();
+        setEditMode(true);
+    }
+
+    function handleFormClose() {
+        setEditMode(false);
+    }
+
     return (
+        <><
+        NavBar openForm={handleFormOpen} />
         <Grid>
             <Grid.Column width={10}>
                 <ActivityList activities={activities} selectedActivity={handleSelectActivity} />
             </Grid.Column>
             <Grid.Column width={6}>
-                {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={handleCancelSelectActivity} />}
-                <ActivityForm />
+                {selectedActivity && <ActivityDetails
+                    activity={selectedActivity}
+                    cancelSelectActivity={handleCancelSelectActivity}
+                    openForm={handleFormOpen} />}
+                {editMode &&
+                    <ActivityForm closeForm={handleFormClose} activity={selectedActivity} />}
             </Grid.Column>
         </Grid>
+        </>
     );
 }
 
