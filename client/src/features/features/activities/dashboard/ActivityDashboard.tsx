b8893052adfd8c6a7f24a15be3c8e158166dda39
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-03-30 02:10:15 +0200

    Update ActivityDashboard.tsx

--- client/src/features/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,33 +1,42 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import ActivityList from "./ActivityList";
+import React, { useEffect, useState } from "react";
 import { Grid } from "semantic-ui-react";
+import ActivityList from "./ActivityList";
 import { Activity } from "../../../../app/models/Activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 
 function Activities() {
     const [activities, setActivities] = useState<Activity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
     useEffect(() => {
-        axios.get('http://localhost:5004/api/Activity')
+        axios.get<Activity[]>('http://localhost:5004/api/Activity')
             .then(response => {
                 setActivities(response.data);
             })
-    }, []);
+    }, []); 
+
+    function handleSelectActivity(id: string) {
+        const selected = activities.find(x => x.id === id);
+        setSelectedActivity(selected);
+    }
+
+    function handleCancelSelectActivity() {
+        setSelectedActivity(undefined);
+    }
 
     return (
         <Grid>
-            <Grid.Column width='10'>
-                <ActivityList activities={activities}/>
+            <Grid.Column width={10}>
+                <ActivityList activities={activities} selectedActivity={handleSelectActivity} />
             </Grid.Column>
-            <Grid.Column width='6'>
-                {activities[0] &&
-                <ActivityDetails activity={activities[0]} />}
+            <Grid.Column width={6}>
+                {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={handleCancelSelectActivity} />}
                 <ActivityForm />
             </Grid.Column>
-        </Grid> 
+        </Grid>
     );
 }
 
-export default Activities; 
+export default Activities;
