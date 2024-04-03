3c2bddcc8745629d0b8df25e283644441ea51ac8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-03 22:16:52 +0300

    Update ActivityForm.tsx

--- client/src/features/features/activities/form/ActivityForm.tsx
+++ client/src/features/features/activities/form/ActivityForm.tsx
@@ -36,7 +36,7 @@ function ActivityForm({closeForm, activity: selectedActivity, createOrEdit}: Pro
                 <Form.Input placeholder='Name' value={activity.name} name='name' onChange={handleInputChange}/>
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}/>
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
-                <Form.Input placeholder='Date' value={activity.data} name='data' onChange={handleInputChange}/>
+                <Form.Input type="date" placeholder='Date' value={activity.data} name='data' onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button onClick={handleSubmit} floated='right' positive type='submit' content='Submit'/>
