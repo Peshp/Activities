760a58518cc5d03c0f5f01a1aebc4dbf06fb6714
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-06 02:56:55 +0300

    Update Edit.cs

--- Activities/Application/Edit.cs
+++ Activities/Application/Edit.cs
@@ -6,9 +6,9 @@ namespace Application
 {
     public class Edit
     {
-        public class Query : IRequest
+        public class Command : IRequest
         {
-            public Query(Activity activity)
+            public Command(Activity activity)
             {
                 Activity = activity;
             }
@@ -16,32 +16,30 @@ namespace Application
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query>
+        public class Handler : IRequestHandler<Command>
         {
-            private ApplicationDbContext _context;
+            private readonly ApplicationDbContext _context;
 
             public Handler(ApplicationDbContext context)
             {
                 _context = context;
             }
 
-            public async Task Handle(Query request, CancellationToken cancellationToken)
+            public async Task Handle(Command request, CancellationToken cancellationToken)
             {
-                Activity change = await _context.Activities.FindAsync(request.Activity.Id);
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-                await Mapper(request, change);
+                if (activity != null)
+                {
+                    activity.Name = request.Activity.Name;
+                    activity.City = request.Activity.City;
+                    activity.Description = request.Activity.Description;
+                    activity.Category = request.Activity.Category;
+                    activity.Date = request.Activity.Date;
+                    activity.Venue = request.Activity.Venue;
+                }
 
                 await _context.SaveChangesAsync();
-            }
-
-            private async Task Mapper(Query request, Activity change)
-            {
-                change.Name = request.Activity.Name;
-                change.City = request.Activity.City;
-                change.Description = request.Activity.Description;
-                change.Category = request.Activity.Category;
-                change.Date = request.Activity.Date;
-                change.Venue = request.Activity.Venue;
             }
         }
     }
