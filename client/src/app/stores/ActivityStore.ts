ab8402537bde6cc1f98b295b393f194f0d3fb708
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-12 00:22:35 +0300

    Update ActivityStore.ts

--- client/src/app/stores/ActivityStore.ts
+++ client/src/app/stores/ActivityStore.ts
@@ -1,16 +1,13 @@
-import { action, makeAutoObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class ActivityStore{
     name = 'Hello from Mobx';
 
     constructor(){
-        makeAutoObservable(this, {
-            name: observable,
-            setName: action.bound
-        });
+        makeAutoObservable(this);
     }
 
-    setName = (name: string) => {
-        this.name = name;
+    setName = () => {
+        this.name = this.name + '!';
     }
 }
