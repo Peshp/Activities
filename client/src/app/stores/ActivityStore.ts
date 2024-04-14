5d7aae4540fcc205537c14f0fbc1d2ab17277e1f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-14 21:33:39 +0300

    Update ActivityStore.ts

--- client/src/app/stores/ActivityStore.ts
+++ client/src/app/stores/ActivityStore.ts
@@ -1,13 +1,55 @@
-import { makeAutoObservable } from "mobx";
+import { action, makeAutoObservable } from "mobx";
+import { Activity } from "../models/Activity";
+import agent from "../api/agent";
 
-export default class ActivityStore{
-    name = 'Hello from Mobx';
+class ActivityStore{
+    activities: Activity[] = [];
+    selectedActivity: Activity | undefined = undefined;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor(){
         makeAutoObservable(this);
     }
 
-    setName = () => {
-        this.name = this.name + '!';
+    loadActivity = async () => {
+        this.setLoadingInitial(true);
+
+        try {
+            const response = await agent.Activities.list();
+            response.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+            })
+            this.setLoadingInitial(false);
+        } catch (error) {
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
+
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activities.find(a => a.id === id);
+        console.log(this.selectedActivity);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    open = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    close = () => {
+        this.editMode = false;
     }
 }
+
+export default ActivityStore;
