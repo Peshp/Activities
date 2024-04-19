c9d42d8d0277a8d0a38519dbb2d0cf43505961f0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-19 19:00:57 +0300

    Create Home.tsx

---
+++ client/src/features/home/Home.tsx
@@ -0,0 +1,12 @@
+import { Container } from "semantic-ui-react";
+
+
+function HomePage(){
+    return(
+        <Container>
+            <h1>Home page</h1>
+        </Container>
+    );
+}
+
+export default HomePage;
