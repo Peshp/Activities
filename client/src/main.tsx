8e9a77670529f62af1628873ba3b13bbc209e1d8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-19 19:01:01 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { StoreContext, store } from './app/stores/store.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/routes/Router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StoreContext.Provider value={store}>
-        <App />
+        <RouterProvider router={router} />
     </StoreContext.Provider>
 )
