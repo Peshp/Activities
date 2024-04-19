902ecd4421301aaeb7412ccece3e793d1470b767
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-19 19:00:53 +0300

    Update Router.tsx

--- client/src/app/routes/Router.tsx
+++ client/src/app/routes/Router.tsx
@@ -1,9 +1,28 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../../App";
+import HomePage from "../../features/home/Home";
+import ActivityDashboard from "../../features/features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/features/activities/form/ActivityForm";
 
-export const routes: RouteObject = {
-    path: "/",
-    element: <App />
-};
+const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/activities",
+          element: <ActivityDashboard />
+        },
+        {
+          path: "/createActivity",
+          element: <ActivityForm />
+        }
+      ]
+    }
+  ];
 
-export const router = createBrowserRouter;
+export const router = createBrowserRouter(routes);
