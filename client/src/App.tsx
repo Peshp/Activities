b3a4e82bbc1550ed552f1a6522ee244685120340
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-04-19 19:00:48 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import 'semantic-ui-css/semantic.min.css';
 import './app/layoult/styles.css';
-import ActivityDashboard from './features/features/activities/dashboard/ActivityDashboard';
-import NavBar from './app/Navbar/NavBar';
 import { Container } from 'semantic-ui-react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './app/Navbar/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <Container style={{ marginTop: '7em' }}> 
-        <ActivityDashboard />
+        <Outlet />
       </Container>
     </>
   )
