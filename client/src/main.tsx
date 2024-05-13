63fc191f436ba5b8ea512e774c886a81ee709b21
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-05-13 14:29:20 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
+import 'react-calendar/dist/Calendar.css'
 import { StoreContext, store } from './app/stores/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/routes/Router.tsx'
