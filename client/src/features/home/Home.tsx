f7ba4cc8c04551c2ca7bd0fb0283e1d51f67bef2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-05-13 14:29:18 +0300

    Update Home.tsx

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -1,12 +1,21 @@
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Segment, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom"; 
 
-
-function HomePage(){
-    return(
-        <Container>
-            <h1>Home page</h1>
-        </Container>
-    );
+function HomePage() {
+  return (
+    <Segment inverted textAlign="center" vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image size='massive' src='assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+          Reactivities
+        </Header>
+        <Header as='h2' inverted content='Welcome to Reactivities' />
+        <Button as={Link} to='/activities' size="huge" inverted>
+          Take me to the Activities
+        </Button>
+      </Container>
+    </Segment>
+  );
 }
 
 export default HomePage;
